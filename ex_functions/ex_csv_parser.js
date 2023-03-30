6c08baeb0c2111cbabfcca16485b50e20bf93d31
Pablo Mariño Boga <pmarino@icarto.es>
2023-03-30 10:13:46 +0200

    Fixed error related to line separator character and minor tweakings

--- ex_functions/ex_csv_parser.js
+++ ex_functions/ex_csv_parser.js
@@ -1,44 +1,55 @@
 const input_file = "source.csv";
 const word_separator = ";";
-const line_separator = "\r\n";
-
+const line_separator = "\n" //"/\r?/\n";
 const fs = require('fs');
 
-function formatCsv(data){
+function printCsv(data) {
   let csv_lines = data.trim().split(line_separator)
-  let csv_words=csv_lines.map((val)=>val.split(word_separator));
-  let column_count=csv_words[0].length;
-  let column_width=new Array(column_count).fill(0);
+  let csv_words = csv_lines.map((val) => val.split(word_separator));
+  let column_width = getColumnsWidth(csv_lines, csv_words);
 
-  // get max width for each column 
-  for(let i=0; i<csv_lines.length;i++){
-    for(let j=0; j<column_count;j++){
+  renderRows(csv_lines, csv_words, column_width);
+}
+
+function getColumnsWidth(csv_lines, csv_words) {
+  let column_count = csv_words[0].length;
+  let column_width = new Array(column_count).fill(0);
+
+  for (let i = 0; i < csv_lines.length; i++) {
+    for (let j = 0; j < column_count; j++) {
       _word_length = csv_words[i][j].length
-      if(column_width[j]<_word_length){
-        column_width[j]=_word_length
+      if (column_width[j] < _word_length) {
+        column_width[j] = _word_length
       }
     }
   }
+  return column_width;
+}
 
-  // render the table
-  for(let i=0; i<csv_lines.length;i++){
-    let line="|"
-    for(let j=0; j<column_count;j++){
-      line+=csv_words[i][j].padEnd(column_width[j]+1, ' ').padStart(1, ' ')+"|"
-       
+function renderRows(csv_lines, csv_words, column_width) {
+  let column_count = csv_words[0].length;
+
+  const END_COLUMN_GAP = 1;
+  for (let i = 0; i < csv_lines.length; i++) {
+    let line = "|"
+    for (let j = 0; j < column_count; j++) {
+      line += csv_words[i][j].padEnd(column_width[j] + END_COLUMN_GAP, ' ').padStart(1, ' ') + "|"
     }
-    
+
+    if (i == 0) {
+      line += renderHorizontalLine(column_count, column_width, END_COLUMN_GAP)
+    }
+
     console.log(line)
-    // add horizontal Line
-    if(i==0){
-      line="+"
-      for(let j=0; j<column_count;j++){
-        line+=String("").padEnd(column_width[j]+1, '-')+"+"
-      }
-      console.log(line)
-    }
   }
+}
 
+function renderHorizontalLine(column_count, column_width, END_COLUMN_GAP) {
+  let line = "\n+"
+  for (let j = 0; j < column_count; j++) {
+    line += String("").padEnd(column_width[j] + END_COLUMN_GAP, '-') + "+"
+  }
+  return line;
 }
 
 fs.readFile(input_file, 'utf8', (err, data) => {
@@ -46,7 +57,5 @@ fs.readFile(input_file, 'utf8', (err, data) => {
     console.error(err);
     return;
   }
-  formatCsv(data)
+  printCsv(data)
 });
-
-
