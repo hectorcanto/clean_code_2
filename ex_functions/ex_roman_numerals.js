68e1de8cf84de3328eddb82c02141c68f208c850
Pablo Mariño Boga <pmarino@icarto.es>
2023-04-13 09:56:28 +0200

    Modifies data structures simplifie code

--- ex_functions/ex_roman_numerals.js
+++ ex_functions/ex_roman_numerals.js
@@ -1,45 +1,22 @@
-convertFromRomanNumerals = function (value) {
-  const arabicNumerals = {
-    I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000,
-  };
-  let total = 0;
-  let previous = 0;
-  for (let i = value.length - 1; i >= 0; i--) {
-    let current = arabicNumerals[value[i]];
-    if (current < previous) {
-      total -= current;
-    } else {
-      total += current;
-    }
-    previous = current;
-  }
-  return total;
-};
-
-
-//console.log(convertFromRomanNumerals("XIV"));
-//console.log(convertFromRomanNumerals("XXIV"));
-//console.log(convertFromRomanNumerals("XXIV"));
-//console.log(convertFromRomanNumerals("MXCIV"));
-
-
+const romanNumerals = [
+  { value: 1000, numeral: "M" },
+  { value: 900, numeral: "CM" },
+  { value: 500, numeral: "D" },
+  { value: 400, numeral: "CD" },
+  { value: 100, numeral: "C" },
+  { value: 90, numeral: "XC" },
+  { value: 50, numeral: "L" },
+  { value: 40, numeral: "XL" },
+  { value: 10, numeral: "X" },
+  { value: 9, numeral: "IX" },
+  { value: 5, numeral: "V" },
+  { value: 4, numeral: "IV" },
+  { value: 1, numeral: "I" },
+];
 
 convertToRomanNumerals = function (value) {
   const lowerLimit = 1;
   const upperLimit = 3999;
-  const alphabet = [
-    "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I",
-  ];
-  const romanNumerals = {
-    M: { value: 1000, numeral: "M" }, CM: { value: 900, numeral: "CM" },
-    D: { value: 500, numeral: "D" }, CD: { value: 400, numeral: "CD" },
-    C: { value: 100, numeral: "C" }, XC: { value: 90, numeral: "XC" },
-    L: { value: 50, numeral: "L" }, XL: { value: 40, numeral: "XL" },
-    X: { value: 10, numeral: "X" }, IX: { value: 9, numeral: "IX" },
-    V: { value: 5, numeral: "V" }, IV: { value: 4, numeral: "IV" },
-    I: { value: 1, numeral: "I" },
-  };
-
   let output = "";
 
   if (value < 1 || value > upperLimit) {
@@ -48,10 +25,10 @@ convertToRomanNumerals = function (value) {
     );
   }
 
-  alphabet.forEach((numeral) => {
-    while (value >= romanNumerals[numeral].value) {
-      output += romanNumerals[numeral].numeral;
-      value -= romanNumerals[numeral].value;
+  romanNumerals.forEach((numeral) => {
+    while (value >= numeral.value) {
+      output += numeral.numeral;
+      value -= numeral.value;
     }
   });
 
@@ -59,6 +36,8 @@ convertToRomanNumerals = function (value) {
 };
 
 /*
+// Some tests
+
 try {
   console.log(convertToRomanNumerals(0));
 } catch (e) {
@@ -73,5 +52,37 @@ try {
 
 console.log(convertToRomanNumerals(2023));
 console.log(convertToRomanNumerals(2949));
+*/
 
+const arabicNumerals = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+
+convertFromRomanNumerals = function (value) {
+  let total = 0;
+  let previous = 0;
+  for (let i = value.length - 1; i >= 0; i--) {
+    let current = arabicNumerals[value[i]];
+    if (current < previous) {
+      total -= current;
+    } else {
+      total += current;
+    }
+    previous = current;
+  }
+  return total;
+};
+
+/*
+//some tests
+console.log(convertFromRomanNumerals("XIV"));
+console.log(convertFromRomanNumerals("XXIV"));
+console.log(convertFromRomanNumerals("XXIV"));
+console.log(convertFromRomanNumerals("MXCIV"));
 */
