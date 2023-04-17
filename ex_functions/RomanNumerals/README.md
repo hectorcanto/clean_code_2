0b758f0db7c89190b2069378a786b1569176295a
Javier <ja7373@gmail.com>
2023-04-17 19:23:32 +0200

    add romanNumerals

---
+++ ex_functions/RomanNumerals/README.md
@@ -0,0 +1,36 @@
+# RomanNumerals
+
+Aplicacion consola realizada con DotNet core.  
+Convierte un número en número romano desde 1 hasta 3000
+
+## Installation
+
+es necesario tener instalado .NET Core  
+[Instalación Linux](https://learn.microsoft.com/en-us/dotnet/core/install/linux)  
+[Instalacion Windows](https://learn.microsoft.com/en-us/dotnet/core/install/windows?tabs=net70)  
+
+
+## Usage
+en la carpeta  donde se encuentra el archivo de proyecto *.csproj ejecutar:  
+```bat
+dotnet run RomanNumerals.csproj 
+```
+pulse 0 para salir  
+
+## Output
+```bat
+Introduzca un número para covertir a número romano, cero o menor para salir
+Introduzca un número: 152
+CLII
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ ex_functions/RomanNumerals/RomanNumerals/Program.cs
@@ -0,0 +1,42 @@
+﻿
+const int EXIT_NUMBER = 0;
+const int INVALID_NUMBER = -1;
+const int MAX_INPUT_NUMBER = 3000;
+string[] unidadesRepo = new string[] { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+string[] decenasRepo = new string[] { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+string[] centenasRepo = new string[] { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+string[] milesRepo = new string[] { "", "M", "MM", "MM", "", "", "", "", "", "" };
+
+Console.Clear();
+Console.WriteLine("Introduzca un número para covertir a número romano, cero o menor para salir");
+
+int inputNumber;
+do
+{
+  Console.Write("Introduzca un número: ");
+  var consoleInput = Console.ReadLine();
+
+  if(Int32.TryParse(consoleInput, out inputNumber) || inputNumber <= MAX_INPUT_NUMBER)
+  {
+    Console.WriteLine(IntToRoman(inputNumber));
+  }
+  else
+  {
+    Console.WriteLine("Número no válido");
+    inputNumber = INVALID_NUMBER;
+  }
+
+
+} while (inputNumber > EXIT_NUMBER);
+
+
+
+string IntToRoman(int num)
+{
+  int unidades = num % 10;
+  int decenas = (num / 10) % 10;
+  int centenas = (num / 100) % 10;
+  int miles = (num / 1000);
+
+  return $"{milesRepo[miles]}{centenasRepo[centenas]}{decenasRepo[decenas]}{unidadesRepo[unidades]}";
+}
